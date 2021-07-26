2dd9665bbefd1b9a984db7d6539ffe82f60ce4b0
Barabashka <vedeninvv@mail.ru>
2021-07-26 16:47:42 +0300

    add creating users for test

--- src/main/java/com/example/demo/service/UserLister.java
+++ src/main/java/com/example/demo/service/UserLister.java
@@ -6,6 +6,7 @@ import com.example.demo.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -15,6 +16,26 @@ public class UserLister {
     @Autowired
     public UserLister(UserRepository userRepository) {
         this.userRepository = userRepository;
+
+        //creating default users just for test
+        if (userRepository.findById(1L).isEmpty()){
+            User user1 = new User();
+            user1.setUsername("user1");
+            user1.setCourses(new HashSet<>());
+            userRepository.save(user1);
+        }
+        if (userRepository.findById(2L).isEmpty()){
+            User user1 = new User();
+            user1.setUsername("user2");
+            user1.setCourses(new HashSet<>());
+            userRepository.save(user1);
+        }
+        if (userRepository.findById(3L).isEmpty()){
+            User user1 = new User();
+            user1.setUsername("user3");
+            user1.setCourses(new HashSet<>());
+            userRepository.save(user1);
+        }
     }
 
     public User userById(Long id){
